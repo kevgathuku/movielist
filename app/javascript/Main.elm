56298b240d1c82c70d6442f1c8ff7cdd3a4daea0
Kevin Gathuku <kevgathuku@gmail.com>
2021-10-14 22:45:06 +0300

    Show Elm content by rendering the Elm pack

--- app/javascript/Main.elm
+++ app/javascript/Main.elm
@@ -1,55 +1,86 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, h1, text)
+import Html exposing (Html, button, div, h1, text)
 import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+
+
 
 -- MODEL
 
+
 type alias Model =
-  {
-  }
+    Int
+
+
 
 -- INIT
 
-init : (Model, Cmd Message)
+
+init : ( Model, Cmd Message )
 init =
-  (Model, Cmd.none)
+    ( 0, Cmd.none )
+
+
 
 -- VIEW
 
+
 view : Model -> Html Message
 view model =
-  -- The inline style is being used for example purposes in order to keep this example simple and
-  -- avoid loading additional resources. Use a proper stylesheet when building your own app.
-  h1 [style "display" "flex", style "justify-content" "center"]
-     [text "Hello Elm!"]
+    -- The inline style is being used for example purposes in order to keep this example simple and
+    -- avoid loading additional resources. Use a proper stylesheet when building your own app.
+    div []
+        [ h1 [ style "display" "flex", style "justify-content" "center" ]
+            [ text ("Hello Elm! " ++ String.fromInt model) ]
+        , button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
+
+
 
 -- MESSAGE
 
+
 type Message
-  = None
+    = Increment
+    | Decrement
+
+
 
 -- UPDATE
 
-update : Message -> Model -> (Model, Cmd Message)
+
+update : Message -> Model -> ( Model, Cmd Message )
 update message model =
-  (model, Cmd.none)
+    case message of
+        Increment ->
+            ( model + 1, Cmd.none )
+
+        Decrement ->
+            ( model - 1, Cmd.none )
+
+
 
 -- SUBSCRIPTIONS
 
+
 subscriptions : Model -> Sub Message
 subscriptions model =
-  Sub.none
+    Sub.none
+
+
 
 -- MAIN
 
+
 main : Program (Maybe {}) Model Message
 main =
-  Browser.element
-    {
-      init = always init,
-      view = view,
-      update = update,
-      subscriptions = subscriptions
-    }
+    Browser.element
+        { init = always init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
